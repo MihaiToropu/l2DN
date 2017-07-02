9a9812528e2cfae3dc441819db26dc80a3683d6b
MihaiToropu <toropumihai@gmail.com>
2017-07-02 23:48:13 +0300

    ...

---
+++ SummerCamp.WebAPI/SummerCamp.WebAPI/Controllers/ReviewsController.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SummerCamp.WebAPI.DataAccess;
+using SummerCamp.WebAPI.Models;
+
+namespace SummerCamp.WebAPI.Controllers
+{
+    public class ReviewsController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetByAnnouncementId(int announcementId)
+        {
+            using (SummerCampDbContext ctx = new SummerCampDbContext())
+            {
+                var data = ctx.Reviews.Where(r => r.AnnouncementId == announcementId).Select(r =>
+                  new
+                  {
+                      Rating = r.Rating,
+                      Comment = r.Comment,
+                      Username = r.Username
+                  }).ToList();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, data);
+                return response;
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage NewReview([FromBody] ReviewCreateDTO review)
+        {
+            if (string.IsNullOrEmpty(review.Comment) && !review.Rating.HasValue)
+            {
+                ModelState.AddModelError("", "Rating or comment required");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Review entity = new Review
+                {
+                    AnnouncementId = review.AnnouncementId,
+                    Comment = review.Comment,
+                    Username = review.Username,
+                    Rating = review.Rating
+                };
+
+                using (SummerCampDbContext ctx = new SummerCampDbContext())
+                {
+                    ctx.Reviews.Add(entity);
+                    ctx.SaveChanges();
+                }
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, review);
+                return response;
+            }
+
+            HttpError error=new HttpError (ModelState, false);
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest, error);
+        }
+    }
+}
---
+++ SummerCamp.WebAPI/SummerCamp.WebAPI/Models/ReviewCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SummerCamp.WebAPI.Models
+{
+    public class ReviewCreateDTO
+    {
+        public int AnnouncementId { get; set; }
+
+        public int? Rating { get; set; }
+
+        public string Comment { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+    }
+}
